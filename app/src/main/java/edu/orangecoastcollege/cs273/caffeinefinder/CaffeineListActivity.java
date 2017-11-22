b86412a0b88848bce7346874c6b92c5d416c0275
Ong <tong@student.cccd.edu>
2017-11-21 16:19:09 -0800

    Initial commit

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/caffeinefinder/CaffeineListActivity.java
@@ -0,0 +1,94 @@
+package edu.orangecoastcollege.cs273.caffeinefinder;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
+
+//TODO: (1) Implement the OnMapReadCallback interface for Google Maps
+//TODO: First, you'll need to compile GoogleMaps in build.gradle
+//TODO: and add permissions and your Google Maps API key in the AndroidManifest.xml
+public class CaffeineListActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private DBHelper db;
+    private List<Location> allLocationsList;
+    private ListView locationsListView;
+    private LocationListAdapter locationListAdapter;
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caffeine_list);
+
+        deleteDatabase(DBHelper.DATABASE_NAME);
+        db = new DBHelper(this);
+        db.importLocationsFromCSV("locations.csv");
+
+        allLocationsList = db.getAllCaffeineLocations();
+        locationsListView = (ListView) findViewById(R.id.locationsListView);
+        locationListAdapter = new LocationListAdapter(this, R.layout.location_list_item, allLocationsList);
+        locationsListView.setAdapter(locationListAdapter);
+
+        //TODO: (2) Load the support map fragment asynchronously
+        //instruct Android to load a Google Map in our fragment
+        SupportMapFragment mapFragment= (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.CaffeinMapFragment);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap= googleMap;
+
+        //CUSTOM MARKER (Big Blue One- my_marker.png)
+
+        LatLng myPosition= new LatLng(33.671028, -117.911305);
+
+        //ADD a custom maekr
+        mMap.addMarker(new MarkerOptions().position(myPosition)
+                .title("My Location").icon(BitmapDescriptorFactory
+                .fromResource(R.drawable.my_marker)));
+        CameraPosition cameraPosition= new CameraPosition.Builder()
+                .target(myPosition).target(myPosition)
+                .zoom(15.0f).build();
+        CameraUpdate cameraUpdate= CameraUpdateFactory.newCameraPosition(cameraPosition);
+
+        mMap.moveCamera(cameraUpdate);
+
+        for(Location nsaLogs: allLocationsList)
+        {
+            LatLng loggedLocation= new LatLng(nsaLogs.getLatitude(),nsaLogs.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(loggedLocation).title(nsaLogs.getName()));
+        }
+        //store the references to the google map in our variable
+//this method is called after the map is loaded from google play services at this point map is ready
+    }
+    public void viewLocationDetails(View v)
+    {
+        LinearLayout selectedLayout= (LinearLayout) v;
+        Intent detailsIntent= new Intent(this, )
+    }
+
+    // TODO: (3) Implement the onMapReady method, which will add a special "marker" for our current location,
+    // TODO: which is 33.671028, -117.911305  (MBCC 139)
+    // TODO: Then add normal markers for all the caffeine locations from the allLocationsList.
+    // TODO: Set the zoom level of the map to 15.0f
+
+
+    // TODO: (4) Create a viewLocationsDetails(View v) method to create a new Intent to the
+    // TODO: CaffeineDetailsActivity class, sending it the selectedLocation the user picked from the locationsListView
+}
